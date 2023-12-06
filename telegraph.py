10211781f276d1617f51c4e08d1a68f0b3274a53
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-12-06 12:04:20 +0530

    Update telegraph.py

--- telegraph.py
+++ telegraph.py
@@ -1,10 +1,8 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import Message
 from bcnadds import TgGraph
-import io
 
 token = "d3b25feccb89e508a9114afb82aa421fe2a9712b963b387cc5ad71e58722"
-
 API_ID = "14688437"
 API_HASH = "5310285db722d1dceb128b88772d53a6"
 BOT_TOKEN = "6162291374:AAEJxgUYtTt0OYDE0G6V2ZhGW-WaLV-qzMQ"
@@ -12,8 +10,8 @@ BOT_TOKEN = "6162291374:AAEJxgUYtTt0OYDE0G6V2ZhGW-WaLV-qzMQ"
 app = Client("telegraph_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 tgraph = TgGraph(access_token=token)
 
-@app.on_message(filters.photo & filters.command("telegraph", prefixes="/") & filters.private)
-async def telegraph_link(client, message):
+@app.on_message(filters.private)
+async def handle_messages(client, message):
     try:
         # Check if the message contains any media (photo, video, document)
         if message.media:
@@ -27,17 +25,20 @@ async def telegraph_link(client, message):
             media_source_url = uploaded_files[0].get('src')
             
             # Send the Telegraph link to the user
-            await message.reply(f"Telegraph link for the media: {media_source_url}")
+            telegraph_response = await tgraph.create_page("Telegraph Page", html_content=f'<img src="{media_source_url}" alt="Telegraph Media">')
+            telegraph_link = f'https://telegra.ph{telegraph_response[0]}'
+            await message.reply(f"Telegraph link for the media: {telegraph_link}")
+            
         elif message.text:
             # If the message contains text, create a Telegraph page with the text
             page_title = "Telegraph Page"
             page_content = message.text
             
             # Create a new Telegraph page
-            response = await tgraph.create_page(page_title, html_content=page_content, return_content=True, return_html=True)
+            telegraph_response = await tgraph.create_page(page_title, html_content=page_content, return_content=True, return_html=True)
             
             # Get the Telegraph link from the response
-            telegraph_link = response.get('url')
+            telegraph_link = f'https://telegra.ph{telegraph_response[0]}'
             
             # Send the Telegraph link to the user
             await message.reply(f"Telegraph link for the text: {telegraph_link}")
